f5ab2c0b1cf1ae5f369ce211b5de10cebae5a9a4
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2025-02-20 16:12:56 +0100

    not using s3 and dynamo for terraform

--- backend.tf
+++ backend.tf
@@ -1,8 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "terraform-lock"
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
