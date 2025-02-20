402a9ecd43eb515b2281c72cce990497ea165859
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2025-02-20 17:03:01 +0100

    removed s3 and dynamo table from terraform, as they already need to be there before running anything

--- backend.tf
+++ backend.tf
@@ -1,3 +1,10 @@
 terraform {
-  backend "local" {}
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-${data.aws_caller_identity.current.account_id}"
+    key            = "terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-lock"
+  }
 }
+
+data "aws_caller_identity" "current" {}
