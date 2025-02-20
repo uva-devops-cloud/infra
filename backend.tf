2e6285822b3a4f20cd95cc73dfc51fb075cb1850
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2025-02-20 17:05:48 +0100

    hardcoded

--- backend.tf
+++ backend.tf
@@ -1,10 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-${data.aws_caller_identity.current.account_id}"
+    bucket         = "my-terraform-state-bucket-686255942173"
     key            = "terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "terraform-lock"
   }
 }
-
-data "aws_caller_identity" "current" {}
