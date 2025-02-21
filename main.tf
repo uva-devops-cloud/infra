582f33df6a34aeaf4d150683febeee8e2f3c0759
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2025-02-21 13:01:20 +0100

    retry with terraform refresh step

--- main.tf
+++ main.tf
@@ -2,6 +2,26 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+# Create an S3 Bucket (for Terraform state storage)
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-terraform-state-bucket-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
+
+data "aws_caller_identity" "current" {}
+
+# DynamoDB for Terraform state locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 # AWS Cognito User Pool
 resource "aws_cognito_user_pool" "students" {
   name = "students-user-pool"
