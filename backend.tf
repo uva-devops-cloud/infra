5132e8e298626ff05c84c940261a21db13790a14
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2025-02-21 13:15:10 +0100

    typo fix

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "my-terraform-state-bucket-686255942173"
     key            = "terraform.tfstate"
     region         = "eu-west-2"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "terraform-locks"
   }
 }
