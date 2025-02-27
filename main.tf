782d299ae7e3eee6589c004f019747eb8665c5aa
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2025-02-27 14:45:18 +0100

    added resource explorer

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+resource "aws_resourceexplorer2_index" "example" {
+  type = "LOCAL"
+}
+
+resource "aws_resourceexplorer2_view" "example_view" {
+  index_arn = aws_resourceexplorer2_index.example.arn
+  name      = "students-infra-view"
+}
+
 # API Gateway (for Lambda endpoints)
 resource "aws_apigatewayv2_api" "api" {
   name          = "students-api"
