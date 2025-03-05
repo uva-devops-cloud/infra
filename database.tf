9837a45ced01c3aa395b607a48bbc7c4d0e72409
jjbush11 <jjbush@uvm.edu>
2025-03-05 21:25:52 +0100

    Database migration done

--- database.tf
+++ database.tf
@@ -114,3 +114,14 @@ output "db_endpoint" {
   description = "RDS endpoint"
   value       = module.rds.db_instance_endpoint
 }
+
+# ----------------------------------------------------------------------------
+# Flyaway migration tool
+# ----------------------------------------------------------------------------
+resource "null_resource" "db_migrations" {
+  depends_on = [module.rds]
+
+  provisioner "local-exec" {
+    command = "flyway -url=jdbc:postgresql://${module.rds.db_instance_endpoint}/studentportal -user=admin -password=${random_password.db_password.result} -locations=filesystem:./migrations migrate"
+  }
+}
