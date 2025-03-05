80015a13ef64f47c90761b6c0288941ecf739674
Merlijn van Uden <34515034+Mrlnvndn@users.noreply.github.com>
2025-03-05 15:46:59 +0100

    Resource Explorer + VPC resources added (#5)
    * added resource explorer
    
    * fix explorer resource definition
    
    * added vpc resources
    
    * expanded the vpc with internet gateway, nat gateway and route tables
    
    * correct availability zone
    
    * temporarily turn off nat_gateway
    
    * updated vpc to no longer use nat gateway

--- variables.tf
+++ variables.tf
@@ -7,3 +7,19 @@ variable "google_client_id" {
 variable "google_client_secret" {
   default = "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"
 }
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "availability_zone" {
+  default = "eu-west-2a"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  default = "10.0.2.0/24"
+}
