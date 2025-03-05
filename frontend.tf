5b16903d809497505196069267665daee1573baa
Mothnep <thomas.penner@student.uva.nl>
2025-03-05 16:15:54 +0100

    Right version of S3+CloudFront

--- frontend.tf
+++ frontend.tf
@@ -1,21 +1,52 @@
 resource "aws_s3_bucket" "frontend_bucket" {
-  bucket = "frontend-unique-bucket-name"
+  bucket = "studentportal-frontend-bucket"
   acl    = "private"
 }
 
-resource "aws_cloudformation_stack" "frontend_stack" {
-  name         = "frontend-cloudformation-stack"
-  template_body = <<TEMPLATE
-{
-  "AWSTemplateFormatVersion": "2010-09-09",
-  "Resources": {
-    "FrontendBucket": {
-      "Type": "AWS::S3::Bucket",
-      "Properties": {
-        "BucketName": "${aws_s3_bucket.frontend_bucket.bucket}"
-      }
+resource "aws_cloudfront_distribution" "frontend_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.frontend_bucket.bucket}.s3.amazonaws.com"
+    origin_id   = "S3-${aws_s3_bucket.frontend_bucket.bucket}"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.frontend_identity.cloudfront_access_identity_path
     }
   }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "StudentPortal Frontend CloudFront Distribution"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.frontend_bucket.bucket}"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
-TEMPLATE
+
+resource "aws_cloudfront_origin_access_identity" "frontend_identity" {
+  comment = "Origin Access Identity for StudentPortal Frontend CloudFront Distribution"
 }
