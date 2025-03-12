b21ae0ddbed759ab58b2f4c6edb1d20b54c3bb63
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2025-03-12 15:57:56 +0100

    test

--- api.tf
+++ api.tf
@@ -229,11 +229,3 @@ resource "aws_api_gateway_stage" "api_stage" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   stage_name    = "v1"
 }
-
-resource "aws_api_gateway_authorizer" "students_authorizer" {
-  name            = "students-authorizer"
-  rest_api_id     = aws_api_gateway_rest_api.api.id
-  type            = "COGNITO_USER_POOLS"
-  provider_arns   = [aws_cognito_user_pool.students.arn]
-  identity_source = "method.request.header.Authorization"
-}
