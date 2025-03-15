64345e913d6fa65bcf8c1effc377e55d3a7b9978
Jan Vlug <jan.vlug@student.uva.nl>
2025-03-15 20:20:47 +0100

    changed db migration s3 bucket to lambda bucket

---
+++ lambda_code_bucket.tf
@@ -0,0 +1,15 @@
+# S3 bucket for storing lambda scripts
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = "lambda-code-${data.aws_caller_identity.current.account_id}"
+  tags   = local.common_tags
+}
+
+# Block public access to migration scripts
+resource "aws_s3_bucket_public_access_block" "lambda_bucket_block" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
