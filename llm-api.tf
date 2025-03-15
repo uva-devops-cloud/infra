0216183a435629cba3f0269872f563dd611e21fa
Jan Vlug <jan.vlug@student.uva.nl>
2025-03-15 19:36:36 +0100

    added lambda function and api endpoint hello + api integrations

---
+++ llm-api.tf
@@ -0,0 +1,36 @@
+#USES THE API DEFINED IN API_GATEWAY
+
+
+# API Gateway resource for the hello endpoint
+resource "aws_api_gateway_resource" "hello" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "hello"
+}
+
+# HTTP GET method for the hello endpoint
+resource "aws_api_gateway_method" "hello_get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.hello.id
+  http_method   = "GET"
+  authorization = "NONE" # Public endpoint for simplicity
+}
+
+# Integration between API Gateway and Lambda
+resource "aws_api_gateway_integration" "hello_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.hello.id
+  http_method             = aws_api_gateway_method.hello_get.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hello_world_lambda.invoke_arn
+}
+
+# Permission for API Gateway to invoke the Lambda
+resource "aws_lambda_permission" "api_gateway_hello" {
+  statement_id  = "AllowAPIGatewayInvokeHello"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_world_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/${aws_api_gateway_method.hello_get.http_method}${aws_api_gateway_resource.hello.path}"
+}
