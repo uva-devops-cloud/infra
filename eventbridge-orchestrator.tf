a06d5e92aafc529062fe43d9adb48b587765775c
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2025-03-16 21:21:40 +0100

    remove unused API Gateway method and integration for query endpoint

--- eventbridge-orchestrator.tf
+++ eventbridge-orchestrator.tf
@@ -27,21 +27,3 @@ resource "aws_cloudwatch_event_target" "worker_response_target" {
     aws_lambda_function.response_aggregator
   ]
 }
-
-# HTTP method for the query endpoint
-resource "aws_api_gateway_method" "query_method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.query_resource.id
-  http_method   = "POST"
-  authorization = "NONE" # Change to "COGNITO_USER_POOLS" if using auth
-}
-
-# Integration for query endpoint with Lambda
-resource "aws_api_gateway_integration" "query_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.query_resource.id
-  http_method             = aws_api_gateway_method.query_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.query_intake.invoke_arn
-}
