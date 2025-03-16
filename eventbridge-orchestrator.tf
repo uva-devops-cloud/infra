a255638cf2f0a1d621269fea84f145f373bacdcf
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2025-03-16 21:19:46 +0100

    removed duplicate resource

--- eventbridge-orchestrator.tf
+++ eventbridge-orchestrator.tf
@@ -28,19 +28,12 @@ resource "aws_cloudwatch_event_target" "worker_response_target" {
   ]
 }
 
-# Add the query API route to the existing REST API Gateway
-resource "aws_api_gateway_resource" "query_resource" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "query"
-}
-
 # HTTP method for the query endpoint
 resource "aws_api_gateway_method" "query_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.query_resource.id
   http_method   = "POST"
-  authorization = "NONE"  # Change to "COGNITO_USER_POOLS" if using auth
+  authorization = "NONE" # Change to "COGNITO_USER_POOLS" if using auth
 }
 
 # Integration for query endpoint with Lambda
