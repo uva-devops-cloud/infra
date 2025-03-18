053f5f6b2a26df36fb38e1d6b3ebf66163c357c5
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2025-03-19 00:45:13 +0100

    asadsf

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -15,27 +15,3 @@ resource "aws_cloudwatch_log_group" "get_student_courses" {
 
   tags = local.common_tags
 }
-
-# Log group for Update Profile Lambda
-resource "aws_cloudwatch_log_group" "update_profile" {
-  name              = "/aws/lambda/${aws_lambda_function.update_profile.function_name}"
-  retention_in_days = 30
-
-  tags = local.common_tags
-}
-
-# Log group for Hello World Lambda
-resource "aws_cloudwatch_log_group" "hello_world" {
-  name              = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
-  retention_in_days = 30
-
-  tags = local.common_tags
-}
-
-# Log group for Get Program Details Lambda
-resource "aws_cloudwatch_log_group" "get_program_details" {
-  name              = "/aws/lambda/${aws_lambda_function.get_program_details.function_name}"
-  retention_in_days = 30
-
-  tags = local.common_tags
-}
